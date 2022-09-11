c54bd7bb168c2d9b414986812fcab9cf26087d6e
stefanaygul <stefan_aygul521@hotmail.com>
2022-09-11 23:40:12 +0300

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.1"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "~> 1.1"
+    }
+  }
+}
+
+provider "azurerm" {
+
+    features {
+
+    }
+}
